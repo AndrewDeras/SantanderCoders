24088d1c9314f3316296b3b154b9154c5b8b9f85
AndrewDeras <andrewderas7@gmail.com>
2023-05-21 22:01:44 -0300

    some codes

--- 04-java/helloWorld/src/Main.java
+++ 04-java/helloWorld/src/Main.java
@@ -1,7 +1,10 @@
-import java.time.LocalDateTime;
-import java.time.LocalDate;
-import java.util.Locale;
-import java.time.format.TextStyle;
+
+// import java.time.LocalDateTime;
+// import java.lang.reflect.Array;
+// import java.time.LocalDate;
+// import java.util.Arrays;
+// import java.util.Locale;
+// import java.time.format.TextStyle;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,28 +31,74 @@ public class Main {
 
         // ola , {nome}. Hoje é {dia da semana}, bom dia/tarde/noite
 
-        String nome = "Andrew";
+        // String nome = "Andrew";
 
-        LocalDate hoje = LocalDate.now();
-        Locale brasil = new Locale.Builder().setLanguage("pt").setRegion("BR").build();
+        // LocalDate hoje = LocalDate.now();
+        // Locale brasil = new
+        // Locale.Builder().setLanguage("pt").setRegion("BR").build();
 
-        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        // String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL,
+        // brasil);
 
-        LocalDateTime agora = LocalDateTime.now();
+        // LocalDateTime agora = LocalDateTime.now();
 
-        String saudacao;
+        // String saudacao;
 
-        if (agora.getHour() >= 0 && agora.getHour() < 12) {
-            saudacao = "Bom dia";
-        } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
-            saudacao = "Boa tarde";
-        }else if (agora.getHour() >= 18 && agora.getHour() < 24){
-            saudacao = "Boa noite";
-        } else{
-            saudacao = "Vamos continuar";
+        // if (agora.getHour() >= 0 && agora.getHour() < 12) {
+        // saudacao = "Bom dia";
+        // } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
+        // saudacao = "Boa tarde";
+        // }else if (agora.getHour() >= 18 && agora.getHour() < 24){
+        // saudacao = "Boa noite";
+        // } else{
+        // saudacao = "Vamos continuar";
+        // }
+
+        // System.out.printf("Ola %s. Hoje é %s, %s.%n", nome, diaSemana,
+        // saudacao.toUpperCase());
+
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println("=========================");
+        // for (int j = 0; j <= 10; j++) {
+        // System.out.println(j + "x" + i + "=" + j * i);
+        // }
+        // }
+
+        // int[] numeros = { 1, 2, 3, 4, 5 };
+
+        // String[] letras = { "a", "b", "c", "d", "e" };
+
+        // for (int i = 0; i < numeros.length; i++) {
+        // System.out.println(numeros[i]);
+        // }
+
+        // System.out.println(Arrays.toString(letras));
+
+        int[] randomNumbers = { 23, 43, 342, 657, 97, 77 };
+        // int maior = randomNumbers[0];
+        // int menor = randomNumbers[0];
+        // int media = 0;
+
+        // for (int i = 0; i < randomNumbers.length; i++) {
+        //     if (randomNumbers[i] > maior) {
+        //         maior = randomNumbers[i];
+        //     }
+        //     if (randomNumbers[i] < menor) {
+        //         menor = randomNumbers[i];
+        //     }
+        //     media += randomNumbers[i];
+        // }
+
+        System.out.println(soma(randomNumbers));
+
+
+    }
+
+    public static int soma (int[] numbers){
+        int soma = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            soma += numbers[i];
         }
-
-        System.out.printf("Olá %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao);
-
+        return soma;
     }
 }
